d35f78ad392167f8e57a3274b8cdd40c192cfcfd
navyayadav123 <120593956+navyayadav123@users.noreply.github.com>
2025-07-29 10:03:31 +0530

    change in admin
    change the admin code in server file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const connectDB = require('./db');
 const authRoutes = require('./routes/authRoutes');
 const expenseRoutes = require('./routes/expenseRoutes');
 //const adminRoutes = require('./routes/adminRoutes')
-const adminRoutes = require('./routes/adminRoutes');
+// const adminRoutes = require('./routes/adminRoutes');
 const incomeRoutes = require('./routes/incomeRoutes');
 const budgetRoutes = require('./routes/budgetRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
@@ -21,7 +21,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/expenses', expenseRoutes);
-app.use('/api/admin', adminRoutes);
+// app.use('/api/admin', adminRoutes);
 app.use('/api/income', incomeRoutes);
 app.use('/api/budget', budgetRoutes);
 app.use('/api/payments', paymentRoutes)
